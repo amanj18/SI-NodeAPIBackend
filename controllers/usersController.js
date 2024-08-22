3c66a8c3c84227462ae6af41ab13af1d870092de
amanj18 <amanjais160@gmail.com>
2024-08-22 17:56:14 +0530

    updated

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -54,4 +54,62 @@ const loginUser = async (req, res) => {
 }
 
 
-export {registerUser , loginUser};
+const getUsers = async (req, res) => {
+    try {
+        const selectQuery = 'SELECT * FROM practice.users';
+        const result = await pool.query(selectQuery);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.log('Error retrieving users:', error.message);
+        res.status(500).json({ error: 'Internal Error' });
+    }
+};
+
+//Get User by id
+
+const getUserById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const selectQuery = `SELECT * FROM practice.users WHERE id = ${id}`;
+        const result = await pool.query(selectQuery);
+        if (result.rowCount === 0) {
+            res.status(404).json({ error: 'User not found' });
+        } else {
+            res.status(200).json(result.rows[0]);
+        }
+    } catch (error) {
+        console.log('Error retrieving user:', error.message);
+        res.status(500).json({ error: 'Internal Error' });
+    }
+};
+
+// Update user
+const updateUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { name, email, password, phonenumber } = req.body;
+        const updateQuery = `UPDATE practice.users SET name = $1, email = $2, password = $3, phonenumber = $4 WHERE id = ${id}`;
+        const values = [name, email, password, phonenumber];
+        const result = await pool.query(updateQuery, values);
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.log('Error updating user:', error.message);
+        res.status(500).json({ error: 'Internal Error' });
+    }
+};
+
+// Delete user
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleteQuery = `DELETE FROM practice.users WHERE id = ${id}`;
+        const result = await pool.query(deleteQuery);
+        res.status(204).json({ message: 'User deleted' });
+    } catch (error) {
+        console.log('Error deleting user:', error.message);
+        res.status(500).json({ error: 'Internal Error' });
+    }
+};
+
+
+export { registerUser, loginUser, getUsers, getUserById, updateUser, deleteUser }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { loginUser, registerUser } from '../controllers/usersController.js'
+import { loginUser, registerUser ,getUsers , getUserById ,updateUser , deleteUser } from '../controllers/usersController.js'
 
 const usersRouter = express.Router()
-usersRouter.post('/',registerUser)
-usersRouter.post('/login',loginUser)
+usersRouter.get('/', getUsers)
+usersRouter.get('/:id', getUserById)
+usersRouter.post('/register', registerUser);
+usersRouter.post('/login', loginUser)
+usersRouter.put('/:id', updateUser)
+usersRouter.delete('/:id', deleteUser)
 
 export default usersRouter;
 
